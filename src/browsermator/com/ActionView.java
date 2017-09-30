465a2d8ceb1bca4a2e76abc945eb919822a3cb2e
Pete <Pete@Bedroom>
2017-09-30 14:43:26 -0700

    1.1.56, fix for action indexes when added at position cont...

--- src/browsermator/com/ActionView.java
+++ src/browsermator/com/ActionView.java
@@ -299,7 +299,7 @@ String stringactionindex = Integer.toString(this.index+1);
    
            this.index = actionindex;
            this.bugindex = bugindex;
-            String stringactionindex = Integer.toString(this.index+1);
+            String stringactionindex = Integer.toString(this.index);
         String stringbugindex = Integer.toString(this.bugindex);
         String bugdashactionindex = stringbugindex + "-" + stringactionindex;
            this.JLabelIndex.setText(bugdashactionindex);
