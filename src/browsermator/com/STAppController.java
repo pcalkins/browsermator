89eeb101c87143e7ad1c4ded29e60685dabca1b0
pcalkins <pcalkins@mvd.com>
2018-02-16 18:14:54 -0800

    1.2.86, fixed bad jars in libs (duplicate files...)

--- src/browsermator/com/STAppController.java
+++ src/browsermator/com/STAppController.java
@@ -57,7 +57,7 @@ public JDesktopPane SeleniumToolDesktop;
 public final String UNIQUE_LOG_DIR;
 public final String USERDIR;
 private int CurrentMDIWindowIndex;
-public final String ProgramVersion = "1.2.85";
+public final String ProgramVersion = "1.2.86";
 public String loginName;
 public String loginPassword;
 String PTPUSERCLOUDDIR;
